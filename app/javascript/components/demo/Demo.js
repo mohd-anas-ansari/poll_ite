f3890cdcb3445224bcb8a665b3150c39f87e06df
anasansari157 <anasansari157@gmail.com>
2020-08-15 17:47:24 +0530

    rendering form react in rails

---
+++ app/javascript/components/demo/Demo.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Demo extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <h1>Rendered from React</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Demo
