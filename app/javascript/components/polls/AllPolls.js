3039a6ac345f134f1adb2de946e40cade6aa96d3
anasansari157 <anasansari157@gmail.com>
2020-08-15 19:12:05 +0530

    add a particular poll to component, Poll

---
+++ app/javascript/components/polls/AllPolls.js
@@ -0,0 +1,20 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+import Poll from "./Poll"
+
+class AllPolls extends React.Component {
+  render() {
+    let polls = this.props.polls
+    return (
+      <React.Fragment>
+        <h1>All Polls</h1>
+        {console.log(this.props.polls)}
+
+        {polls.map(element => <Poll poll={element}/>)}
+      </React.Fragment>
+    );
+  }
+}
+
+export default AllPolls
