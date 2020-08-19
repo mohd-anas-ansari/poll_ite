0bd535cf77df3b71fcc42b2c2516444d9aa0b55e
anasansari157 <anasansari157@gmail.com>
2020-08-19 05:34:50 +0530

    move users table to the top to fix error

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2020_08_18_234024) do
 
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "ballots", force: :cascade do |t|
     t.integer "user_id"
     t.integer "poll_id"
@@ -31,12 +39,7 @@ ActiveRecord::Schema.define(version: 2020_08_18_234024) do
     t.index ["user_id"], name: "index_polls_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+
 
   create_table "votes", force: :cascade do |t|
     t.integer "poll_id"
